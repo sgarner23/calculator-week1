00532fedb861209622d28e3bc3961f04bd55a935
Stephen Garner <stephenallen.garner@gmail.com>
2021-08-23 15:07:23 -0600

    Finished Calculator

--- calc.js
+++ calc.js
@@ -34,10 +34,9 @@ reader.question("What would you like to calculate?", function(input){
 		console.log(num1 - num2);
 	}
 	
-	if (mathSymbol === ""){
-		console.log(num1 * num2);
+	if (mathSymbol === "√"){
+		console.log(Math.sqrt(num1));
 	}
-
 
 	// This line closes the connection to the command line interface.
 	reader.close()
